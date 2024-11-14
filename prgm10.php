9504ab8c447699cf054eb314006c9e1d7d011ee4
anuak01 <168395086+anuak01@users.noreply.github.com>
2024-11-14 09:15:53 +0530

    Add files via upload

--- prgm10.php
+++ prgm10.php
@@ -1,13 +1,9 @@
-10.Write a PHP program to remove duplicates from a sorted list.
-
 <?php
-
-function removeDuplicates ($sortedList) {
+function removeDuplicates($sortedList) {
  $length=count($sortedList);
- if ($length=0) {
-  return $sortedList;
+     if ($length=0) {
+       return $sortedList;
 }
-
 $result = array($sortedList[0]);
 for ($i=1;$i<$length; $i++) {
   if ($sortedList[$i] != $sortedList[Si - 1]) {
@@ -16,12 +12,8 @@ for ($i=1;$i<$length; $i++) {
 }
 return $result;
 }
-
-// Sample sorted list with duplicates 
-$sortedList=arrау(1, 2, 2, 3, 4, 4, 4, 5, 6, 6, 7);
-// Call the removeDuplicates function 
+$sortedList= array(1,2,2,3,4,4,4,5,6,6,7);
 $uniqueList=removeDuplicates($sortedList);
-echo "Original Sorted List:" implode(", ", $sortedList). "\n":
-echo "List with Duplicates Removed: ", implode(", ", $uniqueList). "\n";
-}
+echo "Original Sorted List:".implode(", ", $sortedList). "<br>";
+echo "List with Duplicates Removed: ". implode(", ", $uniqueList). "\n";
 ?>
